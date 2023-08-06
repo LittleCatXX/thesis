99029113c8badca2467b74002719737df59c3b1e
LittleCatXX <83004058+LittleCatXX@users.noreply.github.com>
2023-08-07 00:03:26 +0700

    newUPDATE
    add ngrok
    add host
    test API and chatbot

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
 
       <a class="navbar-brand text-light" href="./scanner.html"> form_test->line </a> <br> <br> <br>
       
-      <a class="navbar-brand text-light" href="./scanAndcheck.php"> ทดสอบระบบ 2 </a>    
+      <a class="navbar-brand text-light" href="./test2.php"> ทดสอบระบบ 2 </a>    
 
     </div>
 
