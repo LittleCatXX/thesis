8f9d9fd45c82c2be58841645e9fc7c0153447622
LittleCatXX <83004058+LittleCatXX@users.noreply.github.com>
2023-08-10 22:31:48 +0700

    update final but not final
    -can reply
    -messeg form line to DB

--- funtionforline.php
+++ funtionforline.php
@@ -1,14 +1,73 @@
 <?php
 
-// Create the bot client instance
-$client = new \GuzzleHttp\Client();
-$config = new \LINE\Clients\MessagingApi\Configuration();
-$config->setAccessToken('<channel access token>');
-$messagingApi = new \LINE\Clients\MessagingApi\Api\MessagingApiApi(
-  client: $client,
-  config: $config,
-);
+
+use \GuzzleHttp\Client;
+use \LINE\Clients\MessagingApi\Configuration;
+use \LINE\Clients\MessagingApi\Api\MessagingApiApi;
+
+function createMessagingApiClient($channelAccessToken) {
+    // Create the Guzzle HTTP client instance
+    $client = new Client();
+
+    // Create the LINE Messaging API configuration instance
+    $config = new Configuration();
+    $config->setAccessToken($channelAccessToken);
+
+    // Create the MessagingApiApi instance
+    $messagingApi = new MessagingApiApi([
+        'client' => $client,
+        'config' => $config,
+    ]);
+
+    return $messagingApi;
+}
 
 
 
+// ใช้ฟังก์ชันเพื่อดึงข้อมูล JSON จากการรับข้อมูล  $events = getJSONDataFromInput();
+function getJSONDataFromInput() {
+  $raw_input = file_get_contents('php://input');
+  $data = json_decode($raw_input, true);
+  return $data;
+}
+
+
+
+
+// ฟังก์ชันสำหรับส่งข้อความผ่าน LINE Messaging API
+function replyToUser($access_token, $user_id, $reply_message) {
+  // ตั้งค่า HTTP headers สำหรับส่งข้อมูลไปยัง LINE Messaging API
+  $headers = array(
+      'Authorization: Bearer ' . $access_token,
+      'Content-Type: application/json'
+  );
+
+  // ข้อมูลที่ต้องการส่งไปยัง LINE Messaging API
+  $data = array(
+      'to' => $user_id, // ค่า USER_ID ของผู้ใช้ Line Chat ที่คุณต้องการส่งข้อความถึง
+      'messages' => array(
+          array(
+              'type' => 'text',
+              'text' => $reply_message
+          )
+      )
+  );
+}
+
+
+// ทำการคิวรีค่า $user_id ที่เก็บในตาราง usersline
+function queryUserIDsFromUsersLine($conn) {
+  $sql_query = "SELECT user_id FROM usersline";
+  $result = $conn->query($sql_query);
+  
+  $user_ids = array();
+  
+  if ($result && $result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+          $user_ids[] = $row['user_id'];
+      }
+  }
+  
+  return $user_ids;
+}
 ?>
---
+++ webhooktest.php
@@ -0,0 +1,85 @@
+<?php
+include 'funtiontest.php';
+include 'db_connection.php';
+
+// Access Token
+$access_token = 'P52UXHhWlbVPXb0ikNPk8135dzvwGrZ92/4cxDrwDlm/iM+VkQ2K1neE6r1ur1dEddlpHANxARzGBpTBPaPmVzVamVTwY8od9++E8Ox8v9VAqb1hg96ttFho4VP67FE2g/dBhkNRIysMAR1MV7VRswdB04t89/1O/w1cDnyilFU=';
+// รับค่าที่ส่งมา
+$content = file_get_contents('php://input');
+// แปลงเป็น JSON
+$events = json_decode($content, true);
+if (!empty($events['events'])) {
+    foreach ($events['events'] as $event) {
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // ข้อความที่ส่งกลับ มาจาก ข้อความที่ส่งมา
+            // ร่วมกับ USER ID ของไลน์ที่เราต้องการใช้ในการตอบกลับ
+            
+            $messages = array(
+                'type' => 'text',
+                'text' => 'Reply message : '.$event['message']['text']."\nUser ID : ".$event['source']['userId'],
+            );
+            $post = json_encode(array(
+                'replyToken' => $event['replyToken'],
+                'messages' => array($messages),
+            ));
+
+
+
+
+
+
+
+
+
+            // URL ของบริการ Replies สำหรับการตอบกลับด้วยข้อความอัตโนมัติ
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer '.$access_token);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            echo $result;
+
+            
+        
+            
+
+            
+            
+
+
+
+
+
+
+
+        }
+
+        $conn = createDBConnection();
+        if (!empty($events['events'])) {
+            foreach ($events['events'] as $event) {
+                // ตรวจสอบว่าเป็นข้อมูลของข้อความ
+                if ($event['type'] === 'message' && $event['message']['type'] === 'text') {
+                    // ดึง UID ของผู้ใช้ที่ส่งข้อความมา
+                    $user_id = $event['source']['userId'];
+                    $message = $event['message']['text'];
+    
+                    // ทำการเก็บ User ID และข้อความที่ผู้ใช้งานส่งมาลงในฐานข้อมูล
+                    $sql = "INSERT INTO usersline (user_id, message_content) VALUES ('$user_id', '$message')";
+                    $conn->query($sql);
+                }
+            }
+        }
+        $conn->close();   
+
+   
+
+
+
+    }
+}
+
